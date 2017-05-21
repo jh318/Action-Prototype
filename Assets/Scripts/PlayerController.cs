3a4bf5df4977a0b3670c86bc5063f75595ff2241
John <jhawley318@gmail.com>
2017-05-21 05:24:15 -0500

    flipped idle, created movement blend tree, overhauled animator tree, sprite flips when moving.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float speed = 3.0f;
+
+	private enum state {idle, moving};
+	private Rigidbody2D body;
+	private Animator animator;
+	private SpriteRenderer sprite;
+
+	void Start(){
+		body = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+		sprite = GetComponent<SpriteRenderer>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		body.velocity = movement * speed;
+
+		bool isMoving = (Mathf.Abs (moveHorizontal)) > 0;
+
+		animator.SetBool ("isMoving", isMoving);
+		if (isMoving) {
+			animator.SetFloat ("moveX", moveHorizontal);
+			transform.localScale = new Vector3 (1.0f * moveHorizontal, 1.0f, 1.0f);
+		}
+
+
+
 	}
+
 }
