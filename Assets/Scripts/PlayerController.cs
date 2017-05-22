f86b81605d219a288bd270f4d1c51b254af10ec0
John <jhawley318@gmail.com>
2017-05-21 23:00:38 -0500

    fixed animation transitions. seems to be smooth now.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour {
 	public enum State {ground, moving, jumping, air,};
 	public State state;
 	public float speed = 3.0f;
-	public float jumpPower = 3.0f;
+	public float jumpForce = 3.0f;
 
 
 	private Rigidbody2D body;
@@ -18,11 +18,13 @@ public class PlayerController : MonoBehaviour {
 		body = GetComponent<Rigidbody2D> ();
 		animator = GetComponent<Animator> ();
 		sprite = GetComponent<SpriteRenderer>();
+		//StartCoroutine ("CheckState");
 	}
 
 
 
 	void FixedUpdate(){
+		Debug.Log (state);
 		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
 		float moveVertical = Input.GetAxisRaw ("Vertical");
 
@@ -31,6 +33,7 @@ public class PlayerController : MonoBehaviour {
 
 		bool isMoving = (Mathf.Abs (moveHorizontal)) > 0;
 		bool isJumping = moveVertical > 0;
+		animator.SetBool ("isJumping", isJumping);
 		bool isCrouching = moveVertical < 0;
 		//bool isAir = false;
 
@@ -40,25 +43,40 @@ public class PlayerController : MonoBehaviour {
 			transform.localScale = new Vector3 (1.0f * moveHorizontal, 1.0f, 1.0f);
 		}
 
-		if(state == State.ground) animator.SetBool("isJumping", isJumping);
+
+
 		if (isJumping && state == State.ground) {
-			animator.SetTrigger ("isJumpingTrigger");
+			animator.SetBool ("isJumping", true);
+			animator.SetBool ("isAir", true);
 			state = State.jumping;
-			//body.AddForce (Vector2.up * jumpPower, ForceMode2D.Impulse);
+			body.AddForce (Vector2.up * jumpForce);
 			Vector3 jumpVelocity = new Vector3(0.0f, 1.0f, 0.0f);
-			body.velocity = jumpVelocity * jumpPower;
+			//body.velocity = jumpVelocity * jumpForce * Time.deltaTime;
 			//isAir = true;
 		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-		if (collision.gameObject.tag == "Ground" && state == State.jumping) {
+		if (collision.gameObject.tag == "Ground") {
 			state = State.ground;
+			animator.SetBool ("isGround", true);
+			animator.SetBool ("isAir", false);
+			animator.SetBool ("isJumping", false);
 		}
 	}
 	void OnCollisionExit2D(Collision2D collision){
-		if (collision.gameObject.tag == "Ground" && state == State.ground) {
+		if (collision.gameObject.tag == "Ground") {
 			state = State.air;
+			animator.SetBool ("isAir", true);
+			animator.SetBool ("isGround", false);
+			animator.SetBool ("isJumping", false);
+		}
+	}
+
+	IEnumerator CheckState(){
+		while (enabled) {
+			Debug.Log (state);
+			yield return new WaitForSeconds (2);
 		}
 	}
 }
